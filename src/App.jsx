9788634072262bc0ad80ac3c7b46aed35f66dfb9
Mario Kreitz <Mario.Kreitz@web.de>
2024-04-15 14:40:57 +0200

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,94 @@
+import React from "react";
+import Drum from "./Drum";
+import "./App.css";
+const audioObjects = [
+  {
+    keyCode: 81,
+    keyTrigger: "Q",
+    id: "Heater-1",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
+  },
+  {
+    keyCode: 87,
+    keyTrigger: "W",
+    id: "Heater-2",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
+  },
+  {
+    keyCode: 69,
+    keyTrigger: "E",
+    id: "Heater-3",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
+  },
+  {
+    keyCode: 65,
+    keyTrigger: "A",
+    id: "Heater-4",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3",
+  },
+  {
+    keyCode: 83,
+    keyTrigger: "S",
+    id: "Clap",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3",
+  },
+  {
+    keyCode: 68,
+    keyTrigger: "D",
+    id: "Open-HH",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
+  },
+  {
+    keyCode: 90,
+    keyTrigger: "Z",
+    id: "Kick-n'-Hat",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
+  },
+  {
+    keyCode: 88,
+    keyTrigger: "X",
+    id: "Kick",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
+  },
+  {
+    keyCode: 67,
+    keyTrigger: "C",
+    id: "Closed-HH",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3",
+  },
+];
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  playSound(event) {
+    const audioObject = audioObjects.find(
+      (obj) => obj.keyTrigger === event.key.toUpperCase()
+    );
+    if (!audioObject) return;
+    document.getElementById(audioObject.keyTrigger).play().catch(console.error);
+    document.getElementById(`drum-${audioObject.keyTrigger}`).focus();
+    document.getElementById("display").innerHTML = audioObject.id;
+  }
+
+  render() {
+    return (
+      <div
+        className="container"
+        id="drum-machine"
+        onKeyDown={this.playSound.bind(this)}
+      >
+        <h1>Drum Machine</h1>
+        <div className="drum-pad-container">
+          {audioObjects.map((obj) => (
+            <Drum audioObject={obj} key={obj.keyTrigger} />
+          ))}
+        </div>
+        <div id="display"></div>
+      </div>
+    );
+  }
+}
+
+export default App;
